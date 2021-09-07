c3e79231757d2ee71982d4770674bb7b82e9c205
Manuel Eduardo Nascimento Antunes <mena1@aluno.ifal.edu.br>
2021-09-07 13:42:52 -0300

    fix: remove unlinted files

--- test/mongo-provider.spec.ts
+++ test/mongo-provider.spec.ts
@@ -1,12 +1,8 @@
 import test from 'japa'
-import { fs, setupApplication } from '../test-helpers'
+import { fs } from '../test-helpers'
 
 test.group('Database Provider', (group) => {
   group.afterEach(async () => {
     await fs.cleanup()
-  })
-
-  test('register mongo provider', async (assert) => {
-    const app = await setupApplication({}, ['../../providers/DatabaseProvider'])
   })
 })
